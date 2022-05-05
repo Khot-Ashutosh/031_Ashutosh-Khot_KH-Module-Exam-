b1b8c29455ffdc82eeca61d290b17dca0d18c12f
Khot-Ashutosh <100849417+Khot-Ashutosh@users.noreply.github.com>
2022-05-05 17:25:31 +0530

    Add files via upload

---
+++ 220340320031/Stack.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class Stack
+{
+	int max = 10;
+	int a1[] = new int [max];
+	
+	static class stack1
+	{
+		int top1;
+		
+		stack1()
+		{
+			top1=-1;
+		}
+	}
+	
+	static class stack2
+	{
+		int top2;
+		
+		stack1()
+		{
+			top2=-1;
+		}
+	}
+	
+	
+	
+	public static void main(String args[])
+	{
+		
+		
+		
+	}
+}
